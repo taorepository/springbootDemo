52065b1b8d60fc2fcbaecc222e7e3c86043ef21f
wangtaotao <898611810@qq.com>
2020-07-06 15:46:58 +0800

    test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,21 +1,21 @@
-package com.example.demo;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import redis.clients.jedis.Jedis;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoApplicationTests {
-
-	private static final Jedis jedis = new Jedis("127.0.0.1",6379);
-	@Test
-	public void contextLoads() {
-		jedis.set("redis","redis");
-		System.out.println(jedis.get("redis"));
-	}
-
-}
+//package com.example.demo;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import redis.clients.jedis.Jedis;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class DemoApplicationTests {
+//
+//	private static final Jedis jedis = new Jedis("127.0.0.1",6379);
+//	@Test
+//	public void contextLoads() {
+//		jedis.set("redis","redis");
+//		System.out.println(jedis.get("redis"));
+//	}
+//
+//}
